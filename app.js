ba70966563e7a0b97b3c22de589d7b02318d0c53
fabiochidda <fabio.chidda99@gmail.com>
2022-02-03 19:13:52 +0100

    Aggiunto prompt per il cognome

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+//Prompt per inserire il nome e stamparlo dentro il titolo
 let userName = prompt("Inserisci qui il tuo nome");
 
 console.log(userName)
 
-document.getElementById('userName').innerHTML = userName;
+document.getElementById('userName').innerHTML = userName; 
+
+//Prompt per inserire il cognome
+let userSurname = prompt("Inserisci qui il tuo cognome");
+
+console.log(userSurname)
